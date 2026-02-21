ed57b4a28fa869f36afa6c84bb73b46eb33740d5
Liujh <Loken315@outlook.com>
2026-02-21 14:21:20 +0000

    feat: finish cachelab

--- labs/cachelab/csim.c
+++ labs/cachelab/csim.c
@@ -1,7 +1,187 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
+typedef struct {
+    int valid;
+    unsigned long tag;
+    int lru_counter;
+} cache_line_t;
+
+typedef struct {
+    cache_line_t *lines;
+} cache_set_t;
+
+typedef struct {
+    cache_set_t *sets;
+    int S;  // number of sets
+    int E;  // lines per set
+    int B;  // block size
+} cache_t;
+
+int hits = 0;
+int misses = 0;
+int evictions = 0;
+int verbose = 0;
+int global_lru = 0;
+
+cache_t* init_cache(int s, int E, int b) {
+    int S = 1 << s;
+    cache_t *cache = malloc(sizeof(cache_t));
+    cache->S = S;
+    cache->E = E;
+    cache->B = 1 << b;
+    cache->sets = malloc(S * sizeof(cache_set_t));
+
+    for (int i = 0; i < S; i++) {
+        cache->sets[i].lines = malloc(E * sizeof(cache_line_t));
+        for (int j = 0; j < E; j++) {
+            cache->sets[i].lines[j].valid = 0;
+            cache->sets[i].lines[j].tag = 0;
+            cache->sets[i].lines[j].lru_counter = 0;
+        }
+    }
+    return cache;
+}
+
+void free_cache(cache_t *cache) {
+    for (int i = 0; i < cache->S; i++) {
+        free(cache->sets[i].lines);
+    }
+    free(cache->sets);
+    free(cache);
+}
+
+void access_cache(cache_t *cache, unsigned long address, int s, int b) {
+    unsigned long set_mask = (1UL << s) - 1;
+    unsigned long set_index = (address >> b) & set_mask;
+    unsigned long tag = address >> (s + b);
+
+    cache_set_t *set = &cache->sets[set_index];
+
+    // Check for hit
+    for (int i = 0; i < cache->E; i++) {
+        if (set->lines[i].valid && set->lines[i].tag == tag) {
+            hits++;
+            set->lines[i].lru_counter = global_lru++;
+            if (verbose) printf("hit ");
+            return;
+        }
+    }
+
+    // Miss
+    misses++;
+    if (verbose) printf("miss ");
+
+    // Find empty line or LRU line
+    int lru_index = 0;
+    int found_empty = 0;
+
+    for (int i = 0; i < cache->E; i++) {
+        if (!set->lines[i].valid) {
+            lru_index = i;
+            found_empty = 1;
+            break;
+        }
+    }
+
+    if (!found_empty) {
+        // Find LRU
+        int min_lru = set->lines[0].lru_counter;
+        for (int i = 1; i < cache->E; i++) {
+            if (set->lines[i].lru_counter < min_lru) {
+                min_lru = set->lines[i].lru_counter;
+                lru_index = i;
+            }
+        }
+        evictions++;
+        if (verbose) printf("eviction ");
+    }
+
+    // Load into cache
+    set->lines[lru_index].valid = 1;
+    set->lines[lru_index].tag = tag;
+    set->lines[lru_index].lru_counter = global_lru++;
+}
+
+void simulate(cache_t *cache, char *trace_file, int s, int b) {
+    FILE *fp = fopen(trace_file, "r");
+    if (!fp) {
+        fprintf(stderr, "Error opening file\n");
+        exit(1);
+    }
+
+    char op;
+    unsigned long address;
+    int size;
+
+    while (fscanf(fp, " %c %lx,%d", &op, &address, &size) == 3) {
+        if (op == 'I') continue;
+
+        if (verbose) printf("%c %lx,%d ", op, address, size);
+
+        if (op == 'L' || op == 'S') {
+            access_cache(cache, address, s, b);
+        } else if (op == 'M') {
+            access_cache(cache, address, s, b);
+            access_cache(cache, address, s, b);
+        }
+
+        if (verbose) printf("\n");
+    }
+
+    fclose(fp);
+}
+
+int main(int argc, char *argv[]) {
+    int s = 0, E = 0, b = 0;
+    char *trace_file = NULL;
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", argv[0]);
+                printf("Options:\n");
+                printf("  -h         Print this help message.\n");
+                printf("  -v         Optional verbose flag.\n");
+                printf("  -s <num>   Number of set index bits.\n");
+                printf("  -E <num>   Number of lines per set.\n");
+                printf("  -b <num>   Number of block offset bits.\n");
+                printf("  -t <file>  Trace file.\n");
+                exit(0);
+            case 'v':
+                verbose = 1;
+                break;
+            case 's':
+                s = atoi(optarg);
+                break;
+            case 'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                b = atoi(optarg);
+                break;
+            case 't':
+                trace_file = optarg;
+                break;
+            default:
+                exit(1);
+        }
+    }
+
+    if (s == 0 || E == 0 || b == 0 || trace_file == NULL) {
+        fprintf(stderr, "Missing required arguments\n");
+        exit(1);
+    }
+
+    cache_t *cache = init_cache(s, E, b);
+    simulate(cache, trace_file, s, b);
+    free_cache(cache);
+
+    printSummary(hits, misses, evictions);
     return 0;
 }
--- labs/cachelab/trans.c
+++ labs/cachelab/trans.c
@@ -22,6 +22,118 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, ii, jj;
+    int v0, v1, v2, v3, v4, v5, v6, v7;
+
+    if (M == 32 && N == 32) {
+        // 32x32: 使用 8x8 分块
+        for (ii = 0; ii < N; ii += 8) {
+            for (jj = 0; jj < M; jj += 8) {
+                for (i = ii; i < ii + 8; i++) {
+                    // 读取一行的 8 个元素到局部变量
+                    v0 = A[i][jj];
+                    v1 = A[i][jj + 1];
+                    v2 = A[i][jj + 2];
+                    v3 = A[i][jj + 3];
+                    v4 = A[i][jj + 4];
+                    v5 = A[i][jj + 5];
+                    v6 = A[i][jj + 6];
+                    v7 = A[i][jj + 7];
+
+                    // 写入到 B 的对应列
+                    B[jj][i] = v0;
+                    B[jj + 1][i] = v1;
+                    B[jj + 2][i] = v2;
+                    B[jj + 3][i] = v3;
+                    B[jj + 4][i] = v4;
+                    B[jj + 5][i] = v5;
+                    B[jj + 6][i] = v6;
+                    B[jj + 7][i] = v7;
+                }
+            }
+        }
+    } else if (M == 64 && N == 64) {
+        // 64x64: 使用 8x8 分块，分成4个4x4子块处理
+        for (ii = 0; ii < N; ii += 8) {
+            for (jj = 0; jj < M; jj += 8) {
+                // 处理上半部分 (4行)
+                for (i = ii; i < ii + 4; i++) {
+                    // 读取 A 的一行 (8个元素)
+                    v0 = A[i][jj];
+                    v1 = A[i][jj + 1];
+                    v2 = A[i][jj + 2];
+                    v3 = A[i][jj + 3];
+                    v4 = A[i][jj + 4];
+                    v5 = A[i][jj + 5];
+                    v6 = A[i][jj + 6];
+                    v7 = A[i][jj + 7];
+
+                    // 左上4x4转置到B的左上
+                    B[jj][i] = v0;
+                    B[jj + 1][i] = v1;
+                    B[jj + 2][i] = v2;
+                    B[jj + 3][i] = v3;
+
+                    // 右上4x4暂存到B的右上（转置位置）
+                    B[jj][i + 4] = v4;
+                    B[jj + 1][i + 4] = v5;
+                    B[jj + 2][i + 4] = v6;
+                    B[jj + 3][i + 4] = v7;
+                }
+
+                // 处理下半部分，同时调整右上的暂存数据
+                for (j = jj; j < jj + 4; j++) {
+                    // 读取B右上的暂存数据
+                    v0 = B[j][ii + 4];
+                    v1 = B[j][ii + 5];
+                    v2 = B[j][ii + 6];
+                    v3 = B[j][ii + 7];
+
+                    // 读取A左下的数据
+                    v4 = A[ii + 4][j];
+                    v5 = A[ii + 5][j];
+                    v6 = A[ii + 6][j];
+                    v7 = A[ii + 7][j];
+
+                    // 将A左下写入B左下
+                    B[j][ii + 4] = v4;
+                    B[j][ii + 5] = v5;
+                    B[j][ii + 6] = v6;
+                    B[j][ii + 7] = v7;
+
+                    // 将暂存数据写入B右上
+                    B[j + 4][ii] = v0;
+                    B[j + 4][ii + 1] = v1;
+                    B[j + 4][ii + 2] = v2;
+                    B[j + 4][ii + 3] = v3;
+                }
+
+                // 处理右下4x4
+                for (i = ii + 4; i < ii + 8; i++) {
+                    v0 = A[i][jj + 4];
+                    v1 = A[i][jj + 5];
+                    v2 = A[i][jj + 6];
+                    v3 = A[i][jj + 7];
+
+                    B[jj + 4][i] = v0;
+                    B[jj + 5][i] = v1;
+                    B[jj + 6][i] = v2;
+                    B[jj + 7][i] = v3;
+                }
+            }
+        }
+    } else {
+        // 61x67 和其他: 使用 16x16 分块
+        for (ii = 0; ii < N; ii += 16) {
+            for (jj = 0; jj < M; jj += 16) {
+                for (i = ii; i < ii + 16 && i < N; i++) {
+                    for (j = jj; j < jj + 16 && j < M; j++) {
+                        B[j][i] = A[i][j];
+                    }
+                }
+            }
+        }
+    }
 }
 
 /* 
